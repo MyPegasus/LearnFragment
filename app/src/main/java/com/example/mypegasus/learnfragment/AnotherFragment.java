78af881b8b2f38234cf1749f393dad756edec8b2
MyPegasus <flying_hbt@163.com>
2015-08-02 23:19:04 +0800

    Android Fragment--1  1. 使用 Fragment

---
+++ app/src/main/java/com/example/mypegasus/learnfragment/AnotherFragment.java
@@ -0,0 +1,29 @@
+package com.example.mypegasus.learnfragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 2015/8/2 0002.
+ */
+public class AnotherFragment extends Fragment {
+//public class AnotherFragment extends android.app.Fragment {
+
+	@Nullable
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//		return super.onCreateView(inflater, container, savedInstanceState);
+		View root = inflater.inflate(R.layout.fragment_another, container, false);
+		root.findViewById(R.id.btn_back).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				getFragmentManager().popBackStack();
+			}
+		});
+		return root;
+	}
+}
---
+++ app/src/main/java/com/example/mypegasus/learnfragment/MainActivityFragment.java
@@ -0,0 +1,41 @@
+package com.example.mypegasus.learnfragment;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+//public class MainActivityFragment extends android.app.Fragment {
+
+	public MainActivityFragment() {
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+							 Bundle savedInstanceState) {
+		View root = inflater.inflate(R.layout.fragment_main, container, false);
+
+		root.findViewById(R.id.btn_showAnotherFragment).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				/*FragmentManager fm = getFragmentManager();
+				FragmentTransaction ts = fm.beginTransaction();
+				ts.hide(MainActivityFragment.this);
+				ts.add(R.id.fragment, new AnotherFragment());
+				ts.addToBackStack(null);
+				ts.commit();*/
+				getFragmentManager().beginTransaction().
+						addToBackStack(null).
+						replace(R.id.fragment, new AnotherFragment()).
+						commit();
+			}
+		});
+
+		return root;
+	}
+}
